a502481f1d172d3ce746a48c848ffbe480d31703
Berrysoft <Strawberry_Str@hotmail.com>
2022-01-13 22:44:13 +0800

    Complete IO.

--- src/main.cu
+++ src/main.cu
@@ -1,5 +1,8 @@
 #include <fsmp.cuh>
 
+#include <thrust/device_vector.h>
+#include <thrust/host_vector.h>
+
 #include <curand.h>
 #include <curand_kernel.h>
 
@@ -7,8 +10,9 @@
 #include <boost/program_options.hpp>
 #include <iostream>
 #include <numeric>
+#include <vector>
 
-static constexpr std::size_t TRIALS = 2000;
+static constexpr std::uint32_t TRIALS = 2000;
 
 enum fsmp_step : int
 {
@@ -47,18 +51,19 @@ __device__ __host__ void* memmove_back(void* dest, const void* src, size_t count
 }
 
 __device__ void flow_one(
-    const std::size_t nw,
-    const std::size_t nl,
+    const std::uint32_t nw,
+    const std::uint32_t nl,
     float* __restrict__ cx, // nw x nl, Cov^-1 * A, 详见 FBMP
     const float* __restrict__ tlist, // nl
     float* __restrict__ z, // nw, residue waveform
     const float mus, // spe波形的平均幅值
     const float sig2s, // spe波形幅值方差
+    const float sig2w,
     const float* __restrict__ A, // nw x nl
     const float* __restrict__ p_cha, // nl
     const float* __restrict__ c_cha, // nl + 1, with 0 padding in front
     const float mu_t, // LucyDDM 的估算 PE 数
-    float* __restrict__ s0_history, // TRIALS, ||s||_0
+    std::uint32_t* __restrict__ s0_history, // TRIALS, ||s||_0
     float* __restrict__ delta_nu_history, // TRIALS, Δν
     float* __restrict__ t0_history, // TRIALS, t_0
     // Temp buffers
@@ -82,13 +87,13 @@ __device__ void flow_one(
     curandState_t rnd_state;
     curand_init(id, 0, 0, &rnd_state);
 
-    const std::size_t trials_nslice = div_ceil<std::size_t>(TRIALS, blockDim.x);
-    const std::size_t n2 = nw * nl;
-    const std::size_t n2_nslice = div_ceil<std::size_t>(n2, blockDim.x);
+    const std::uint32_t trials_nslice = div_ceil(TRIALS, blockDim.x);
+    const std::uint32_t n2 = nw * nl;
+    const std::uint32_t n2_nslice = div_ceil(n2, blockDim.x);
 
-    for (std::size_t i = 0; i < trials_nslice; i++)
+    for (std::uint32_t i = 0; i < trials_nslice; i++)
     {
-        std::size_t index = id + i * blockDim.x;
+        std::uint32_t index = id + i * blockDim.x;
         if (index < TRIALS)
         {
             istar[index] = curand_uniform(&rnd_state);
@@ -96,10 +101,20 @@ __device__ void flow_one(
         }
     }
 
+    // cx is initialized as A
+    for (std::uint32_t i = 0; i < n2_nslice; i++)
+    {
+        std::uint32_t index = id + i * blockDim.x;
+        if (index < n2)
+        {
+            cx[index] /= sig2w;
+        }
+    }
+
     __syncthreads();
     interp_by(TRIALS, nl + 1, home_s, c_cha);
 
-    std::size_t NPE0 = (std::size_t)(mu_t + 0.5);
+    std::uint32_t NPE0 = (std::uint32_t)(mu_t + 0.5);
     float log_mu = std::log(mu_t);
 
     __shared__ float s[1024];
@@ -112,20 +127,20 @@ __device__ void flow_one(
         s[id] = 0;
     }
     assert(NPE0 < 1024);
-    std::size_t len_s = NPE0;
+    std::uint32_t len_s = NPE0;
     interp_by(NPE0, nl + 1, s, c_cha);
     __syncthreads();
 
     __shared__ float delta_nu;
     __shared__ float beta;
-    for (std::size_t i = 0; i < NPE0; i++)
+    for (std::uint32_t i = 0; i < NPE0; i++)
     {
         combine(nw, nl, A, cx, s[i], A_vec, c_vec);
         __syncthreads();
         move(nw, nl, A_vec, c_vec, z, 1, mus, sig2s, A, &delta_nu, delta_cx, delta_z);
-        for (std::size_t j = 0; j < n2_nslice; j++)
+        for (std::uint32_t j = 0; j < n2_nslice; j++)
         {
-            std::size_t jndex = id + j * blockDim.x;
+            std::uint32_t jndex = id + j * blockDim.x;
             if (jndex < n2)
             {
                 cx[jndex] += delta_cx[jndex];
@@ -151,9 +166,9 @@ __device__ void flow_one(
         }
     }
 
-    for (std::size_t i = 0; i < trials_nslice; i++)
+    for (std::uint32_t i = 0; i < trials_nslice; i++)
     {
-        std::size_t index = id + i * blockDim.x;
+        std::uint32_t index = id + i * blockDim.x;
         if (index < TRIALS)
         {
             flip[index] = choose_step(curand_uniform(&rnd_state));
@@ -167,7 +182,7 @@ __device__ void flow_one(
     __shared__ float p1[1024];
     __shared__ float new_p1[1024];
     __shared__ float temp_p1[1024];
-    for (std::size_t i = 0; i < TRIALS; i++)
+    for (std::uint32_t i = 0; i < TRIALS; i++)
     {
         __syncthreads();
 
@@ -240,7 +255,7 @@ __device__ void flow_one(
                 move1(nw, nl, A_vec, c_vec, z, step, mus, sig2s, &delta_nu, &beta);
                 if (id == 0)
                 {
-                    delta_nu += log_mu + lc(real_time(home, nl, tlist) - t0) - logf(p_cha[(std::size_t)home]) - logf(len_s + 1);
+                    delta_nu += log_mu + lc(real_time(home, nl, tlist) - t0) - logf(p_cha[(std::uint32_t)home]) - logf(len_s + 1);
                 }
                 __syncthreads();
                 if (delta_nu >= accept)
@@ -260,7 +275,7 @@ __device__ void flow_one(
         }
         else
         {
-            std::size_t op = (std::size_t)(t * len_s);
+            std::uint32_t op = (std::uint32_t)(t * len_s);
             float loc = s[op];
             combine(nw, nl, A, cx, loc, A_vec, c_vec);
             __syncthreads();
@@ -270,7 +285,7 @@ __device__ void flow_one(
             {
                 if (id == 0)
                 {
-                    delta_nu -= log_mu + p1[op] - logf(p_cha[(std::size_t)loc]) - logf(len_s);
+                    delta_nu -= log_mu + p1[op] - logf(p_cha[(std::uint32_t)loc]) - logf(len_s);
                 }
                 __syncthreads();
                 if (delta_nu >= accept)
@@ -289,9 +304,9 @@ __device__ void flow_one(
                 if (nloc >= 0.5 && nloc <= nl - 0.5)
                 {
                     move2(nw, nl, A_vec, c_vec, annihilate, mus, A, beta, delta_cx, delta_z);
-                    for (std::size_t j = 0; j < n2_nslice; j++)
+                    for (std::uint32_t j = 0; j < n2_nslice; j++)
                     {
-                        std::size_t jndex = id + j * blockDim.x;
+                        std::uint32_t jndex = id + j * blockDim.x;
                         if (jndex < n2)
                         {
                             temp_cx[jndex] = cx[jndex] + delta_cx[jndex];
@@ -323,9 +338,9 @@ __device__ void flow_one(
                         {
                             s[op] = nloc;
                         }
-                        for (std::size_t j = 0; j < n2_nslice; j++)
+                        for (std::uint32_t j = 0; j < n2_nslice; j++)
                         {
-                            std::size_t jndex = id + j * blockDim.x;
+                            std::uint32_t jndex = id + j * blockDim.x;
                             if (jndex < n2)
                             {
                                 delta_cx[jndex] += temp_cx[jndex];
@@ -351,9 +366,9 @@ __device__ void flow_one(
 
         if (delta_nu >= accept)
         {
-            for (std::size_t j = 0; j < n2_nslice; j++)
+            for (std::uint32_t j = 0; j < n2_nslice; j++)
             {
-                std::size_t jndex = id + j * blockDim.x;
+                std::uint32_t jndex = id + j * blockDim.x;
                 if (jndex < n2)
                 {
                     cx[jndex] += delta_cx[jndex];
@@ -375,7 +390,9 @@ __device__ void flow_one(
 
         if (id == 0)
         {
+            s0_history[i] = len_s;
             delta_nu_history[i] = delta_nu;
+            t0_history[i] = t0;
             flip[i] = step;
         }
     }
@@ -385,18 +402,19 @@ __global__ void flow(
     const std::size_t NW,
     const std::size_t mnw, // max nw
     const std::size_t mnl, // max nl
-    const std::size_t* pnw,
-    const std::size_t* pnl,
+    const std::uint32_t* pnw,
+    const std::uint32_t* pnl,
     float* __restrict__ cx, // NW x mnw x mnl
     const float* __restrict__ tlist, // NW x mnl
     float* __restrict__ z, // NW x mnw
-    const float mus,
-    const float sig2s,
+    const float* __restrict__ mus,
+    const float* __restrict__ sig2s,
+    const float* __restrict__ sig2w,
     const float* __restrict__ A, // NW x mnw x mnl
     const float* __restrict__ p_cha, // NW x mnl
     const float* __restrict__ c_cha, // NW x (mnl + 1)
-    const float* mu_t, // NW
-    float* __restrict__ s0_history, // NW x TRIALS
+    const float* __restrict__ mu_t, // NW
+    std::uint32_t* __restrict__ s0_history, // NW x TRIALS
     float* __restrict__ delta_nu_history, // NW x TRIALS
     float* __restrict__ t0_history, // NW x TRIALS
     // Temp buffers
@@ -427,8 +445,9 @@ __global__ void flow(
             cx + wid * mn2,
             tlist + wid * mnl,
             z + wid * mnw,
-            mus,
-            sig2s,
+            mus[wid],
+            sig2s[wid],
+            sig2w[wid],
             A + wid * mn2,
             p_cha + wid * mnl,
             c_cha + wid * (mnl + 1),
@@ -459,8 +478,7 @@ int main(int argc, char** argv)
     po::options_description program("TurboSMP");
     program.add_options()(
         "output,o", po::value<std::string>()->required(), "Output file")(
-        "input,i", po::value<std::string>()->required(), "Input file")(
-        "ref,r", po::value<std::string>()->required(), "Reference file");
+        "input,i", po::value<std::string>()->required(), "Input file");
     try
     {
         po::store(po::parse_command_line(argc, argv, program), vm);
@@ -475,15 +493,147 @@ int main(int argc, char** argv)
 
     auto input_name = vm["input"].as<std::string>();
     auto output_name = vm["output"].as<std::string>();
-    auto ref_name = vm["ref"].as<std::string>();
 
     auto input = H5::H5File(input_name, H5F_ACC_RDONLY);
-    auto ref = H5::H5File(ref_name, H5F_ACC_RDONLY);
+
+    auto float_type = H5::PredType::NATIVE_FLOAT;
+
+    auto A_data = input.openDataSet("A");
+    auto A_space = A_data.getSpace();
+    hsize_t A_size[3];
+    A_space.getSimpleExtentDims(A_size);
+    std::size_t NW = A_size[0];
+    std::size_t mnw = A_size[1];
+    std::size_t mnl = A_size[2];
+
+    std::vector<float> A(NW * mnw * mnl, 0.0f);
+    A_data.read(A.data(), float_type);
+
+    std::vector<float> z(NW * mnw, 0.0f);
+    input.openDataSet("z").read(z.data(), float_type);
+
+    auto tq_data = input.openDataSet("tq");
+
+    std::vector<float> tlist(NW * mnl, 0.0f);
+    std::vector<float> p_cha(NW * mnl, 0.0f);
+    {
+        H5::CompType t(sizeof(float));
+        t.insertMember("t_s", 0, float_type);
+        tq_data.read(tlist.data(), t);
+    }
+    {
+        H5::CompType t(sizeof(float));
+        t.insertMember("q_s", 0, float_type);
+        tq_data.read(p_cha.data(), t);
+    }
+
+    std::vector<float> c_cha(NW * (mnl + 1), 0.0f);
+    for (std::size_t i = 0; i < NW; i++)
+    {
+        std::partial_sum(p_cha.begin() + i * mnl, p_cha.end() + (i + 1) * mnl, c_cha.begin() + i * (mnl + 1) + 1);
+    }
+
+    auto index_data = input.openDataSet("index");
+
+    std::vector<float> mu_t(NW, 0.0f);
+    std::vector<std::uint32_t> nw(NW, 0);
+    std::vector<std::uint32_t> nl(NW, 0);
+    std::vector<float> mus(NW, 0.0f);
+    std::vector<float> sig2s(NW, 0.0f);
+    std::vector<float> sig2w(NW, 0.0f);
+
+    {
+        H5::CompType t(sizeof(float));
+        t.insertMember("mu0", 0, float_type);
+        index_data.read(mu_t.data(), t);
+    }
+    {
+        H5::CompType t(sizeof(std::uint32_t));
+        t.insertMember("l_wave", 0, H5::PredType::NATIVE_UINT32);
+        index_data.read(nw.data(), t);
+    }
+    {
+        H5::CompType t(sizeof(std::uint32_t));
+        t.insertMember("l_t", 0, H5::PredType::NATIVE_UINT32);
+        index_data.read(nl.data(), t);
+    }
+    {
+        H5::CompType t(sizeof(float));
+        t.insertMember("mus", 0, float_type);
+        index_data.read(mus.data(), t);
+    }
+    {
+        H5::CompType t(sizeof(float));
+        t.insertMember("sig2s", 0, float_type);
+        index_data.read(sig2s.data(), t);
+    }
+    {
+        H5::CompType t(sizeof(float));
+        t.insertMember("sig2w", 0, float_type);
+        index_data.read(sig2w.data(), t);
+    }
+
+    thrust::device_vector<std::uint32_t> dnw = nw;
+    thrust::device_vector<std::uint32_t> dnl = nl;
+    thrust::device_vector<float> dcx = A;
+    thrust::device_vector<float> dtlist = tlist;
+    thrust::device_vector<float> dz = z;
+    thrust::device_vector<float> dA = A;
+    thrust::device_vector<float> dp_cha = p_cha;
+    thrust::device_vector<float> dc_cha = c_cha;
+    thrust::device_vector<float> dmu_t = mu_t;
+    thrust::device_vector<float> dmus = mus;
+    thrust::device_vector<float> dsig2s = sig2s;
+    thrust::device_vector<float> dsig2w = sig2w;
+
+    // returns
+    thrust::device_vector<std::uint32_t> s0_history(NW * TRIALS, 0.0f);
+    thrust::device_vector<float> delta_nu_history(NW * TRIALS, 0.0f);
+    thrust::device_vector<float> t0_history(NW * TRIALS, 0.0f);
+
+    // buffers
+    thrust::device_vector<float> istar(NW * TRIALS, 0.0f);
+    thrust::device_vector<float> home_s(NW * TRIALS, 0.0f);
+    thrust::device_vector<float> A_vec(NW * mnw, 0.0f);
+    thrust::device_vector<float> c_vec(NW * mnw, 0.0f);
+    thrust::device_vector<float> delta_cx(NW * mnw * mnl, 0.0f);
+    thrust::device_vector<float> delta_z(NW * mnw, 0.0f);
+    thrust::device_vector<float> temp_cx(NW * mnw * mnl, 0.0f);
+    thrust::device_vector<float> temp_z(NW * mnw, 0.0f);
+    thrust::device_vector<float> wanders(NW * TRIALS, 0.0f);
+    thrust::device_vector<float> wts(NW * TRIALS, 0.0f);
+    thrust::device_vector<float> accepts(NW * TRIALS, 0.0f);
+    thrust::device_vector<float> accts(NW * TRIALS, 0.0f);
+    thrust::device_vector<fsmp_step> flip(NW * TRIALS, none);
+
+    flow CUDA_KERNEL(NW, 1024)(
+        NW, mnw, mnl,
+        dnw.data().get(), dnl.data().get(),
+        dcx.data().get(), dtlist.data().get(), dz.data().get(),
+        dmus.data().get(), dsig2s.data().get(), dsig2w.data().get(),
+        dA.data().get(), dp_cha.data().get(), dc_cha.data().get(),
+        dmu_t.data().get(),
+        s0_history.data().get(), delta_nu_history.data().get(), t0_history.data().get(),
+        istar.data().get(), home_s.data().get(),
+        A_vec.data().get(), c_vec.data().get(),
+        delta_cx.data().get(), delta_z.data().get(),
+        temp_cx.data().get(), temp_z.data().get(),
+        wanders.data().get(), wts.data().get(),
+        accepts.data().get(), accts.data().get(),
+        flip.data().get());
+
+    thrust::host_vector<float> host_s0 = s0_history;
+    thrust::host_vector<float> host_nu = delta_nu_history;
+    thrust::host_vector<float> host_t0 = t0_history;
+
     auto output = H5::H5File(output_name, H5F_ACC_TRUNC);
 
-    // TODO: read files
-    // TODO: call kernel
-    // TODO: write output
+    hsize_t len_history = TRIALS;
+    H5::DataSpace space(1, &len_history);
+
+    output.createDataSet("s0", H5::PredType::NATIVE_UINT32, space).write(host_s0.data(), H5::PredType::NATIVE_UINT32, space);
+    output.createDataSet("delta_nu", float_type, space).write(host_nu.data(), float_type, space);
+    output.createDataSet("t0", float_type, space).write(host_t0.data(), float_type, space);
 
     return 0;
 }
